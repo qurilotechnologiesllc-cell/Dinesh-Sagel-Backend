0aabedf593f927b2c809b35eca56d7e8f5948b99
Pritam Sharma <qurilotechnologiesllc@gmail.com>
2026-05-21 10:35:31 +0530

    update video plan model

--- src/models/videoPlan.model.js
+++ src/models/videoPlan.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const videoPlanSchema = new Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
-    duration: { type: Number, required: true }, // Duration in seconds
+    duration: { type: String, required: true }, // Duration in seconds
 }, { timestamps: true });
 
 const VideoPlan = model('VideoPlan', videoPlanSchema);
